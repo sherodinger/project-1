1d95a8744bdeaa201891410dec5178a449b6d3bd
Kourosh <yarandik@yahoo.com>
2018-12-17 14:49:33 +0330

    js

---
+++ index.js
@@ -0,0 +1,17 @@
+function mashinhesab(a,b) {
+    var jam= a+b;
+    var menha=a-b;
+    var zarb= a*b;
+    var taghsim = a/b;
+
+    document.write("in baraye jame "+jam +"</br>");
+    document.write("in baraye menha"+menha+"</br>");
+    document.write("in baraye zarbe"+zarb+"</br>");
+    document.write("inam baraye taghsim"+taghsim+"</br>");
+
+}
+
+
+
+
+mashinhesab(2,3);
